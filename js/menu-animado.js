c36f83df1c7ce3ff2cd9bfc07a1c772231e4db3a
Ivaneudo <159584361+Ivaneudo@users.noreply.github.com>
2024-11-04 12:54:53 -0300

    responsividade

---
+++ js/menu-animado.js
@@ -0,0 +1,10 @@
+function BtnAnimado(){
+    const menuSection = document.querySelector('#menu-mobile')
+    const btn = document.querySelector('.btn');
+    const cadastra_se = document.querySelector('.titulo');
+    menuSection.classList.toggle('aparecerrr')
+    btn.classList.toggle('ativar');
+    cadastra_se.classList.toggle('sumir');
+
+    
+}
