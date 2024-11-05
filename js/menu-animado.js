cee5d3909a694f3506a799e8b3a194bfa0291172
Ivaneudo <159584361+Ivaneudo@users.noreply.github.com>
2024-11-05 13:50:39 -0300

    finalização

--- js/menu-animado.js
+++ js/menu-animado.js
@@ -4,7 +4,5 @@ function BtnAnimado(){
     const cadastra_se = document.querySelector('.titulo');
     menuSection.classList.toggle('aparecerrr')
     btn.classList.toggle('ativar');
-    cadastra_se.classList.toggle('sumir');
-
-    
+    cadastra_se.classList.toggle('sumir');   
 }
